e2c1ac2d484bc80458a93421711a92365ee44ed4
Avebave <arvid.hedstrom@elev.ga.ntig.se>
2024-10-18 09:41:06 +0200

    Co-authored-by: hector salamanca <anckepancke@users.noreply.github.com> Co-authored-by: Olle Axelsson <butterdogo@users.noreply.github.com>

--- hiss.py
+++ hiss.py
@@ -1,8 +1,92 @@
-currentlevel = 1
-direction = True
-elevatorButtons[10]
+import random
+import time
+
+direction = True #true = upp, false = neråt
+elevatorButtons = [10]
+run = 1
+
+floor = 1
+previousFloor = 0
+procedure = 1
+targetFloor = 1
+loopInt = 0
+
+print (f'hissen är på våning: {floor}')
 
 
-for x in range(10):{
+#Subjekt nedan här
 
-}
+subjektArray = [1,2,3,4,5]
+subjektTarget =[1,2,3,4,5]
+subjektArraySort = [1,2,3,4,5]
+subjektTargetSort =[1,2,3,4,5]
+
+while (loopInt < 5):
+    subjektArray[loopInt] = random.randint(1,10)
+    print(loopInt)
+    print(subjektArray[loopInt])
+    loopInt = loopInt + 1
+loopInt = 0
+while (loopInt < 5):
+    subjektTarget[loopInt] = random.randint(1,10)
+    print(loopInt)
+    print(subjektTarget[loopInt])
+    loopInt = loopInt + 1
+
+subjektArraySort = subjektArray
+subjektTargetSort = subjektTarget
+
+print(subjektArray)
+print(subjektTarget)
+subjektArraySort.sort()
+subjektTargetSort.sort()
+print(subjektArraySort)
+print(subjektTargetSort)
+
+
+# GÖR EN COPY AV ARRAY OK
+
+#hiss skit
+while (run == 1):
+    try:
+        floor = int(input(""))
+    except ValueError:
+        print("Skriv rätt din idiot")
+    if floor == 500:
+        print("välkommen till secret våning")
+    if floor != 500:
+        if floor > 10 or floor < 1:
+            print("Det finns endast 10 våningar")
+            floor = previousFloor
+    
+    if previousFloor > floor:
+        procedure = previousFloor
+        while (procedure > floor):
+            procedure = procedure - 1
+            print (f'|----|\n| 00 | hissen är på våning: {procedure}\n|----|\n')
+            time.sleep(0.5)
+    if previousFloor < floor:
+        procedure = previousFloor
+        while (procedure < floor):
+            procedure = procedure + 1
+            print (f'|----|\n| 00 | hissen är på våning: {procedure}\n|----|\n')
+            time.sleep(0.5)
+    time.sleep(0.5)
+    previousFloor = floor
+
+while ():
+    if previousFloor > floor:
+        procedure = previousFloor
+        while (procedure > floor):
+            procedure = procedure - 1
+            print (f'|----|\n| 00 | hissen är på våning: {procedure}\n|----|\n')
+            time.sleep(0.5)
+
+if previousFloor < floor:
+    procedure = previousFloor
+    while (procedure < floor):
+        procedure = procedure + 1
+        print (f'|----|\n| 00 | hissen är på våning: {procedure}\n|----|\n')
+        time.sleep(0.5)
+time.sleep(0.5)
+previousFloor = floor
---
+++ subjekt.py
@@ -0,0 +1,8 @@
+import math
+import random
+
+names = ["Arvid", "Sam", "Herman", "Olle"]
+levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for x in levels:
+  print(x)
